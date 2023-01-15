d99924845a2d574933cd07156740e9b921eeba00
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-15 18:55:53 +0300

    Initial Commit

---
+++ HeroWidgetApp/HeroWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetAppApp.swift
+//  HeroWidgetApp
+//
+//  Created by Sezer on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
